a8a17041e5f4dc9065583a90b5fa36dc6c923873
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-10-13 23:34:31 +0300

    Прописал действие входа в аккаунт

--- src/store/actions/auth.ts
+++ src/store/actions/auth.ts
@@ -1,5 +1,34 @@
-import { Types } from "./types";
+import { Dispatch } from "redux";
+import { actionType } from "../../types/action";
 
-export const login = () => {};
+import { Actions } from "./types";
+import axios from "axios";
+
+export const login =
+	(login: string, password: string) =>
+	async (dipatch: Dispatch<actionType>) => {
+		if (
+			login === process.env.REACT_APP_LOGIN &&
+			password === process.env.REACT_APP_PASSWORD
+		) {
+			try {
+				const response = await axios.post(
+					process.env.REACT_APP_LOGIN_API as string
+				);
+
+				return dipatch({
+					type: Actions.AUTH_SUCCESS,
+					payload: response.data,
+				});
+			} catch (err) {
+				return dipatch({
+					type: Actions.AUTH_FAILURE,
+				});
+			}
+		}
+		return dipatch({
+			type: Actions.AUTH_FAILURE,
+		});
+	};
 
 export const logout = () => {};
