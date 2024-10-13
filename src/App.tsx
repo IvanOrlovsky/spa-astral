b0aa276dedf96abc3e24428e2aacbbff56c1702c
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-10-13 12:03:55 +0300

    Сделал простейшую навигацию

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Cards from "./pages/Cards";
 
-const App = () => {
-	return <div></div>;
-};
+const App = () => (
+	<Routes>
+		<Route path="/" element={<Home />} />
+		<Route path="/login" element={<Login />} />
+		<Route path="/profile" element={<Profile />} />
+		<Route path="/cards" element={<Cards />} />
+	</Routes>
+);
 
 export default App;
