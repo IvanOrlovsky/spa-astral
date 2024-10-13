7d8c9481ff1a8716352478d0dbb5fcab63bda4ac
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-10-13 22:04:52 +0300

    Почему-то редьюсер возвращал функцию, а не состояние, исправил

--- src/store/reducers/auth.ts
+++ src/store/reducers/auth.ts
@@ -1,32 +1,35 @@
 import { actionType } from "../../types/action";
 import { Types } from "../actions/types";
 
-const defaultState: {
+type stateType = {
 	accessToken: string | null;
-} = {
+};
+
+const defaultState: stateType = {
 	accessToken: null,
 };
 
-export function authReducer() {
-	return (state = defaultState, action: actionType) => {
-		switch (action.type) {
-			case Types.AUTH_SUCCESS:
-				return {
-					...state,
-					accessToken: action.payload,
-				};
-			case Types.AUTH_FAILURE:
-				return {
-					...state,
-					accessToken: null,
-				};
-			case Types.LOGOUT:
-				return {
-					...state,
-					accessToken: null,
-				};
-			default:
-				return state;
-		}
-	};
+export function authReducer(
+	state = defaultState,
+	action: actionType
+): stateType {
+	switch (action.type) {
+		case Types.AUTH_SUCCESS:
+			return {
+				...state,
+				accessToken: action.payload,
+			};
+		case Types.AUTH_FAILURE:
+			return {
+				...state,
+				accessToken: null,
+			};
+		case Types.LOGOUT:
+			return {
+				...state,
+				accessToken: null,
+			};
+		default:
+			return state;
+	}
 }
