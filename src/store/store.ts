552525b1617293ae688f48d4f2bab80c1dc60d3a
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-10-13 23:27:45 +0300

    Добавил thunk в middleware и определил нужные типы

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,16 @@
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
 import { rootReducer } from "./reducers/rootReduser";
 import { composeWithDevTools } from "@redux-devtools/extension";
 
-export const store = createStore(rootReducer, composeWithDevTools());
+export const store = createStore(
+	rootReducer,
+	composeWithDevTools(applyMiddleware(thunk))
+);
+
+// Определяем тип хранилища redux, там определены
+export type AppStore = typeof store;
+// Через утилитный тип определяем тип хранимых данных возвращаемых из метода getState()
+export type RootState = ReturnType<AppStore["getState"]>;
+// Определяем тип метода dipatch()
+export type AppDispatch = AppStore["dispatch"];
