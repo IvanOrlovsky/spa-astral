ac58fe11d72bbacd80c12a4d20275033d7984ddc
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-10-13 17:30:34 +0300

    Подключил redux-devtools для отладки состояний

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
 import { legacy_createStore as createStore } from "redux";
 import { rootReducer } from "./reducers/rootReduser";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, composeWithDevTools());
