72e15cd1f81dbe84f1838b72c6daa13b41ab1f0a
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-10-14 14:14:00 +0300

    Сделал страницу карточек с сеткой

--- src/pages/Cards.tsx
+++ src/pages/Cards.tsx
@@ -1,5 +1,34 @@
+import Grid from "@mui/material/Grid2";
+
 import Layout from "../components/Layout";
+import { useAppDispatch, useAppSelector } from "../hooks/hooks";
+import { useEffect } from "react";
+import { getCards } from "../store/actions/cards";
+
+import WordCard from "../components/WordCard";
 
 export default function Cards() {
-	return <Layout>Cards</Layout>;
+	const dispatch = useAppDispatch();
+
+	const cards = useAppSelector((state) => state.cards.cards);
+
+	useEffect(() => {
+		dispatch(getCards());
+	}, []);
+
+	return (
+		<Layout>
+			<Grid
+				container
+				spacing={{ xs: 2, md: 3 }}
+				columns={{ xs: 4, sm: 8, md: 12 }}
+			>
+				{cards.map((card, index) => (
+					<Grid key={index} size={{ xs: 2, sm: 4, md: 4 }}>
+						<WordCard {...card} />
+					</Grid>
+				))}
+			</Grid>
+		</Layout>
+	);
 }
