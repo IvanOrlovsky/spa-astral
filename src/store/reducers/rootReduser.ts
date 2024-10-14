5dae8d60af13c032a6c5a93b087c203a1a16170b
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-10-14 14:14:14 +0300

    Скомбинировал отправителей

--- src/store/reducers/rootReduser.ts
+++ src/store/reducers/rootReduser.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { authReducer } from "./auth";
+import { cardsReducer } from "./cards";
 
 export const rootReducer = combineReducers({
 	auth: authReducer,
+	cards: cardsReducer,
 });
